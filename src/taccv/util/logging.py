b08472e1bbc15deda98bb1a0d4884f94ee4171b1
Foxhunter Labs <joewells636@gmail.com>
2025-12-24 17:31:18 -0500

    Add logging configuration with structlog

---
+++ src/taccv/util/logging.py
@@ -0,0 +1,11 @@
+import structlog
+
+def configure_logging():
+    structlog.configure(
+        processors=[
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ]
+    )
+    return structlog.get_logger()
